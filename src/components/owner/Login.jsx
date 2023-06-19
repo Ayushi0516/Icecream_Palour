166c4c4ce274e740329e2e985115d0f9a6fd7e49
Ayushi0516 <ayushigupta0516@gmail.com>
2023-06-19 22:23:11 +0530

    login form

--- src/components/owner/Login.jsx
+++ src/components/owner/Login.jsx
@@ -1,53 +1,121 @@
-import { Box, Button, Flex, FormControl, FormLabel, Heading, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ErrorMessage from "./ErrorMessage";
 // display="block" margin="auto"
 const Login = () => {
-  const [email,setEmail]=useState("")
-  const [password,setPassword]=useState("")
-  const [error,setError]=useState(false)
-  const navigate=useNavigate()
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleSubmit=()=>{
-    if(!email || !password){
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [loading,setLoading]=useState(false)
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!form.email || !form.password) {
       setError(true);
+      setForm(" ")
       return;
-    }
-    else{
-      setError(false)
-      navigate("/owner/inventory")
-    }
-  }
+    } else {
+      try{
+        setLoading(true)
+        fetch("https://reqres.in/api/login", {
+          method: "POST",
+          headers: {
+            "content-Type": "application/json"
+          },
+          body: JSON.stringify(form)
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            setForm(res)
+            setLoading(false)
+            navigate("/owner/inventory");
+          })
+      }
+      catch(err){
+        setLoading(false);
+        setError(true);
+        console.log(err);
+        setForm("")
+      }
+     
+  
+     }
+  };
   return (
-    
-    <Box w="full"  mt="55" align="center" justifyContent="center" >
-    <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
-      <Box textAlign="center">
-        <Heading>Login</Heading>
-      </Box>
-      <Box my={4} textAlign="left"  >
-        
-        <form>
-          <FormControl  >
-            {/* <ErrorMessage>fill all the fields</ErrorMessage> */}
-          {error && <div>fill</div> }
-            <FormLabel>Email</FormLabel>
-            <Input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}     placeholder="test@test.com" />
-           
-          </FormControl>
-          <FormControl mt={3}>
-            <FormLabel>Password</FormLabel>
-            <Input type="password"  value={password} onChange={(e)=>setPassword(e.target.value)}  placeholder="*******" />
-          </FormControl>
-          <Button  onClick={handleSubmit} width="full" colorScheme="blue" mt={4} type="submit">
-            Login
-          </Button>
-        </form>
+    <Box w="full" mt="55" align="center" justifyContent="center">
+      <Box
+        p={8}
+        maxWidth="500px"
+        borderWidth={1}
+        borderRadius={8}
+        boxShadow="lg"
+      >
+        <Box textAlign="center">
+          <Heading>Login</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form onSubmit={handleSubmit}>
+            {error && <ErrorMessage message={"Please fill out all fields"} />}
+            <FormControl >
+              {/* <ErrorMessage>fill all the fields</ErrorMessage> */}
+           {/* Invalid Username & Password */}
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                placeholder="test@test.com"
+              />
+            </FormControl>
+            <FormControl mt={3} >
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                name="password"
+                value={form.password}
+                onChange={handleChange}
+                placeholder="*******"
+              />
+            </FormControl>
+            <Button width="full" colorScheme="blue" mt={4} type="submit">
+             {loading ? (
+              <CircularProgress isIndeterminate size="24px" color="teal" />
+            ) : (
+              'Login'
+            )}
+            
+             
+            </Button>
+          </form>
+        </Box>
       </Box>
     </Box>
-  </Box>
-);
+  );
 };
 
 export default Login;
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export const AuthContext = React.createContext();
+
+function AuthContextProvider({ children }) {
+  const [state, setState] = useState({
+    isAuth: false,
+    token: null
+  });
+  function handleLogin(token) {
+    setState({
+      ...state,
+      isAuth: true,
+      token: token
+    });
+  }
+  function handleLogout() {
+    setState({
+      ...state,
+      isAuth: true,
+      token: null
+    });
+  }
+  return (
+    <AuthContext.Provider value={{ state, handleLogout, handleLogin }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
